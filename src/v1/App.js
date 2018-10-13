2b877918a087345b6051b0d693562e7d22dcede7
Christopher Skoblenick <irworkinghard@gmail.com>
2018-10-12 23:18:18 -0400

    Simplified v1; added style sheet

--- src/v1/App.js
+++ src/v1/App.js
@@ -11,18 +11,19 @@ class App extends Component {
       <div className="TodoApp">
         <h1>Todo List App</h1>
         <ul>
-          {this.todos.map(todo => 
-            <li 
-              key={todo}
-              tabIndex={0}
-            >
-              {todo}
-            </li>
-          )}
+          {this.todos.map(makeListItem)}
         </ul>
       </div>
     );
   }
+}
+
+function makeListItem(text, key) {
+  return (
+    <li key={key}>
+      {text} 
+    </li>
+  );
 }
 
 export default App;
